39f035dbecb4c57c4552a331297f7fd5e43c093e
Ayla1524 <aylasilva1524@outlook.com>
2022-06-14 23:50:03 -0300

    adicioanando codigo

---
+++ minehr/teste/src/App.js
@@ -0,0 +1,13 @@
+import Scatter from "./Components/Scatter/Scatter";
+import Barra from "./Components/Bar/Barra";
+
+function App() {
+  return (
+    <div>
+      <Barra />
+      <Scatter />
+    </div>
+  );
+}
+
+export default App;
---
+++ minehr/teste/src/Components/Bar/Barra.js
@@ -0,0 +1,50 @@
+import React from "react";
+import ReactApexChart from "react-apexcharts";
+
+function Bar() {
+  const series = [
+    {
+      data: [400, 430, 448, 470, 540, 580, 690, 1100, 1200, 1380],
+    },
+  ];
+  const options = {
+    chart: {
+      type: "bar",
+      height: 350,
+    },
+    plotOptions: {
+      bar: {
+        borderRadius: 4,
+        horizontal: true,
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    xaxis: {
+      categories: [
+        "South Korea",
+        "Canada",
+        "United Kingdom",
+        "Netherlands",
+        "Italy",
+        "France",
+        "Japan",
+        "United States",
+        "China",
+        "Germany",
+      ],
+    },
+  };
+
+  return (
+    <ReactApexChart
+      series={series}
+      options={options}
+      type={"bar"}
+      height={350}
+    />
+  );
+}
+
+export default Bar;
